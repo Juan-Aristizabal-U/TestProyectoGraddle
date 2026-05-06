f7eaaee6eb5ac442d238416ec1c737e7f4ddcc70
Juan Aristizabal <juan.fernando.aristizabal@correounivalle.edu.co>
2026-05-06 17:19:18 -0500

    Implementacion de lo visto en clase (creacion de pacquetes y controladores, para guardar informacion en el formulario

---
+++ src/main/java/co/edu/uvpalmira/fpo/web/ControladorAsignatura.java
@@ -0,0 +1,27 @@
+package co.edu.uvpalmira.fpo.web;
+
+import co.edu.uvpalmira.fpo.web.modelo.Asignatura;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sala Sistemas
+ */
+@Named(value = "asignCtrl")
+@SessionScoped
+public class ControladorAsignatura implements Serializable {
+    
+    private Asignatura asignatura = new Asignatura("750014C", "FPOE", (byte)3, (byte)3);
+    
+    public Asignatura getAsignatura(){
+        return asignatura;
+    }
+    
+}
---
+++ src/main/java/co/edu/uvpalmira/fpo/web/modelo/Asignatura.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.uvpalmira.fpo.web.modelo;
+
+/**
+ *
+ * @author Sala Sistemas
+ */
+public class Asignatura {
+    
+    //Atributos propios
+    private String codigo; 
+    private String nombre;
+    private byte creditos;
+    private byte intensidad;
+
+    //Contructor
+    public Asignatura(String codigo, String nombre, byte creditos, byte intensidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.intensidad = intensidad;
+    }
+
+    //Metodos Get
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public byte getCreditos() {
+        return creditos;
+    }
+
+    public byte getIntensidad() {
+        return intensidad;
+    }
+
+    
+    
+}
